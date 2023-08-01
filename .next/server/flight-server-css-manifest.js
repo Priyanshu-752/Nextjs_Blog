94e2fca70353b5322d81119c446dabcd56c5706e
PRIYANSHU NEGI <priyanshunegi752@gmail.com>
2023-08-01 10:18:36 +0530

    Finsh

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,30 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\layout.tsx": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\page": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\create\\page": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\register\\page": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\login\\page": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\blogs\\[blogId]\\page": [
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\app\\globals.css",
+      "C:\\Users\\USER\\Desktop\\blog_nextjs\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
